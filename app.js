dafd46575d9bb3c5f5da7c516d58571540f9cde5
JeanCarloPeralta <58532380+JeanCarloPeralta@users.noreply.github.com>
2021-06-28 05:47:30 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path'); 
+const app = express();
+const port = 3000;
+
+const home = require('./routes/home.js');
+const pets = require('./routes/pets.js');
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/home',home);
+app.use('/pets', pets);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
